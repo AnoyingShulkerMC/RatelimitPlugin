3451197874ba0131fe997ac3c3788d3e75282a18
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2020-08-03 11:21:50 -0700

    Update main.java

--- src/main/java/ratelimit/main.java
+++ src/main/java/ratelimit/main.java
@@ -1,11 +1,13 @@
 package ratelimit;
 
 import arc.Events;
+import arc.util.Log;
 import arc.util.Timer;
 import mindustry.Vars;
 import mindustry.entities.type.Player;
 import mindustry.game.EventType;
 import mindustry.plugin.Plugin;
+import mindustry.world.blocks.logic.MessageBlock;
 
 import java.util.HashMap;
 
@@ -23,6 +25,16 @@ public class main extends Plugin {
             TempPlayerData tdata = PlayerDataGroup.getOrDefault(e.player.uuid,null);
             if(tdata == null) return;
             tdata.eventsPerSecond++;
+
+        });
+        Events.on(EventType.BlockBuildEndEvent.class, (e) -> {
+            if(e.player == null) return;
+            if(e.breaking) return;
+            TempPlayerData tdata = (PlayerDataGroup.getOrDefault(e.player.uuid, null));
+            if(tdata == null) return;
+            if(e.tile.block().name.equals("message")){
+                tdata.messageBlocksPer10Seconds++;
+            }
         });
         Events.on(EventType.PlayerLeave.class, (e) -> {
             PlayerDataGroup.remove(e.player.uuid);
@@ -40,10 +52,20 @@ public class main extends Plugin {
             for(Player player:Vars.playerGroup) {
                 TempPlayerData tdata = PlayerDataGroup.getOrDefault(player.uuid,null);
                 if(tdata == null) return;
+                tdata.messageBlocksPer10Seconds = 0;
+            }
+        },0,10);
+        Timer.schedule(() -> {
+            for(Player player:Vars.playerGroup) {
+                TempPlayerData tdata = PlayerDataGroup.getOrDefault(player.uuid,null);
+                if(tdata == null) return;
                 if(tdata.interactUntil > 0) {
                     tdata.interactUntil--;
                 }
-
+                //building 15 message blocks per 10 seconds is kinda alot
+                if(tdata.messageBlocksPer10Seconds > 15) {
+                    player.con.close();
+                }
                 if(tdata.eventsPerSecond >= minEventsPerSec){
                     player.sendMessage("[scarlet]You can't interact for 10 seconds because you exceeded rate limit");
                     tdata.interactUntil = 10;
