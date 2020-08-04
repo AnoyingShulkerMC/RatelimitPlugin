e3c03baa5c32e3c8d65f7c2803b00ede9953ccfd
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2020-08-03 17:58:08 -0700

    Update TempPlayerData.java

--- src/main/java/ratelimit/TempPlayerData.java
+++ src/main/java/ratelimit/TempPlayerData.java
@@ -4,4 +4,5 @@ public class TempPlayerData {
     public int eventsPerSecond = 0;
     public int interactUntil = 0;
     public int messageBlocksPer10Seconds = 0;
+    public int rateLimitsExceeded = 0;
 }
